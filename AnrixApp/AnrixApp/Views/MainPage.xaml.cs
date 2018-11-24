fb687e7559b98531630f33d35696a6a2c5291d3c
Volkov Andrey <derbylive145@gmail.com>
2018-11-24 20:21:00 +0300

    Create app structure

---
+++ AnrixApp/AnrixApp/Views/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
+using Xamarin.Forms.PlatformConfiguration;
+
+namespace AnrixApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : Xamarin.Forms.TabbedPage
+    {
+        public MainPage()
+        {
+            On<Android>().SetToolbarPlacement(ToolbarPlacement.Bottom);
+            InitializeComponent();
+        }
+    }
+}
