10ccb916567e81870e90cdf2b48abd04c20a30d3
Volkov Andrey <derbylive145@gmail.com>
2018-12-05 19:13:34 +0300

    Implemented SettingsPage

--- AnrixApp/AnrixApp/ViewModels/GroupsPage.xaml.cs
+++ AnrixApp/AnrixApp/ViewModels/GroupsPage.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using AnrixApp.Models;
 using AnrixApp.Services;
+using AnrixApp.Views;
 using Plugin.FilePicker;
 using Plugin.FilePicker.Abstractions;
+using Plugin.Settings;
 using System;
+
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -32,6 +35,9 @@ namespace AnrixApp.ViewModels
             {
                 FileData filedata = await CrossFilePicker.Current.PickFile();
                 var faculty = FileReaderService.ReadFromFile(filedata.FilePath);
+
+                await CrossFilePicker.Current.SaveFile(filedata);
+
                 OnListUpdated(faculty);
 
             }
@@ -45,6 +51,11 @@ namespace AnrixApp.ViewModels
         {
             var faculty = MockFacultyData.getMockicngFaculty();
             OnListUpdated(faculty);
+        }
+
+        private async void ToolbarItem_Clicked_2(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SettingsPage());
         }
 
         private async void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
