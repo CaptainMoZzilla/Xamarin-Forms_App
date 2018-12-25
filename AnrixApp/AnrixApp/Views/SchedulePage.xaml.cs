98ab351d99ef92e3e03349c1db1a6aa582ad3aa9
Volkov Andrey <derbylive145@gmail.com>
2018-12-25 11:43:17 +0300

    Improved loading UI

--- AnrixApp/AnrixApp/Views/SchedulePage.xaml.cs
+++ AnrixApp/AnrixApp/Views/SchedulePage.xaml.cs
@@ -13,6 +13,7 @@ namespace AnrixApp.Views
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class SchedulePage : ContentPage
 	{
+        private string GroupNumber;
         private string BASE_UI = "https://iis.bsuir.by/#/schedule;groupName=";
 
         public SchedulePage ()
@@ -24,7 +25,27 @@ namespace AnrixApp.Views
         {
             InitializeComponent();
             WebView1.Source = BASE_UI + groupNumber;
-            //WebView1.Reload();
+            GroupNumber = groupNumber;
         }
-	}
+
+        private void WebView1_Navigated(object sender, WebNavigatedEventArgs e)
+        {
+            Title = GroupNumber;
+            Animation.Pause();
+            Animation.IsVisible = false;
+            WebView1.IsVisible = true;
+        }
+
+        private void WebView1_Navigating(object sender, WebNavigatingEventArgs e)
+        {
+            Title = "Loading...";
+                WebView1.IsVisible = false;
+            if (!Animation.IsVisible)
+                Animation.IsVisible = true;
+            if (Animation.IsTabStop)
+                Animation.IsTabStop = false;
+            if (!Animation.IsPlaying)
+                Animation.Play();
+        }
+    }
 }
